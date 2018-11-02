0e11e567ae80c3b6e926fcc9e172d93ebbe8c61d
Hemant Upadhyay <hemantupadhyay59@gmail.com>
2018-11-02 16:57:52 +0530

    Initial SPFx extension commit

---
+++ SP-SPFx-Extn/src/extensions/dynamicPageCreateAndAddWebPart/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IDynamicPageCreateAndAddWebPartApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'DynamicPageCreateAndAddWebPartApplicationCustomizerStrings' {
+  const strings: IDynamicPageCreateAndAddWebPartApplicationCustomizerStrings;
+  export = strings;
+}
