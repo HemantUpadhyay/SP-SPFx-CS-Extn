da2a6000672fc9e62a9e51b1345f26c602412500
Hemant Upadhyay <hemantupadhyay59@gmail.com>
2018-11-05 18:25:58 +0530

    Calls for Azure function code check in

--- SP-SPFx-Extn/src/extensions/dynamicPageCreateAndAddWebPart/DynamicPageCreateAndAddWebPartApplicationCustomizer.ts
+++ SP-SPFx-Extn/src/extensions/dynamicPageCreateAndAddWebPart/DynamicPageCreateAndAddWebPartApplicationCustomizer.ts
@@ -1,3 +1,4 @@
+import { AadHttpClient, HttpClientResponse } from '@microsoft/sp-http';
 import { override } from '@microsoft/decorators';
 import { Log } from '@microsoft/sp-core-library';
 import {
@@ -25,15 +26,84 @@ export default class DynamicPageCreateAndAddWebPartApplicationCustomizer
 
   @override
   public onInit(): Promise<void> {
-    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+    // Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
 
-    let message: string = this.properties.testMessage;
-    if (!message) {
-      message = '(No properties were provided.)';
-    }
+    // let message: string = this.properties.testMessage;
+    // if (!message) {
+    //   message = '(No properties were provided.)';
+    // }
 
-    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
-
+    // Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
+    this.Initiate();
     return Promise.resolve();
+  }
+
+  @override
+  public onRender(): void{
+      
+  }
+
+  private async getNewPageStatus() {
+    // this.context.aadHttpClientFactory
+    //   .getClient('https://tenant.onmicrosoft.com/6b347c27-f360-47ac-b4d4-af78d0da4223')
+    //   .then((client: AadHttpClient): void => {
+    //     client
+    //       .get('https://myfunction.azurewebsites.net/api/CurrentUser', AadHttpClient.configurations.v1)
+    //       .then((response: HttpClientResponse): Promise<JSON> => {
+    //         return response.json();
+    //       })          
+    //   });
+
+    const currentWebUrl: string = this.context.pageContext.web.absoluteUrl;
+    //const pageName = 'DynamicPage.aspx'
+    var functionUrl : string = "https://[functionName].azurewebsites.net/api/[FunctionMethod1]";
+
+    var functionPageCreation : string = "https://[functionName].azurewebservices.net/api/[FunctionMethod2]";
+    const requestHeaders: Headers = new Headers();
+    requestHeaders.append("Content-type", "application/json");
+    requestHeaders.append("Cache-Control", "private"); 
+
+    const postOptions : RequestInit = {
+    headers: requestHeaders,
+    //body: `{\r\n    siteURL: '${currentWebUrl}',\r\n    pageName: '${pageName}' \r\n}`,
+    body: `{\r\n    siteURL: '${currentWebUrl}'\r\n}`,
+    method: "POST"
+    };
+    let responseText: string = "";
+    let createPageStatus: string = "";
+    console.log("Wait started for Creating page");
+    await fetch(functionUrl, postOptions).then((response) => {
+        console.log("Response returned");
+        if (response.ok) {
+          return response.json()
+
+
+        }
+        else
+        {
+            var errMsg = "Error detected while adding site page. Server response wasn't OK ";
+            console.log(errMsg);
+        } 
+      }).then((responseJSON: JSON) => {
+        responseText = JSON.stringify(responseJSON).trim();
+        console.log(responseText);
+        if(responseText.toLowerCase().indexOf("success") > 0)
+            {
+              console.log("success feedback");
+            }
+        if(responseText.toLowerCase().indexOf("error") > 0)
+            {
+              console.log("web call errored");
+            }
+      }
+    ).catch ((response: any) => {
+      let errMsg: string = `WARNING - error when calling URL ${functionUrl}. Error = ${response.message}`;
+      console.log(errMsg);
+    });
+    console.log("wait finished");
+  }
+
+  private async Initiate() {
+    await this.getNewPageStatus();
   }
 }
